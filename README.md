897f00acb44f758722c8ddbc489e7853bc307a7a
mikeyo98 <mikeyo98@users.noreply.github.com>
2022-08-13 15:15:34 -0700

    Initial upload

---
+++ README.md
@@ -0,0 +1,6 @@
+Dodong music bot with fixed playlist search and an additional command that allows users to select a text channel for music messages
+
+Updates will only be about my personal preferences for this bot
+Currently no plans on adding new features
+
+For full description and installation, see Dodong by nize (https://github.com/nizewn/Dodong)
---
+++ commands/musicchannel.js
@@ -0,0 +1,44 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+const { TextChannel } = require("discord.js");
+const Command = require("../structures/command.js")
+
+module.exports = new Command({
+    name: "musicchannel",
+    aliases: ['mc'],
+    description: "Selects a channel for playing musics",
+    permission: "ADMINISTRATOR",
+    options: [
+        {
+            description: 'Channel name',
+            name: 'channel',
+            required: false,
+            type: 3
+        }
+    ],
+    async run(message, args, client, slash){
+
+        if(!args[0]) {
+            console.log("Reply to commands");
+            client.musicchannel = null;
+            const embed = new EmbedBuilder();
+            embed.setDescription(`No channel selected, ${client.user.username} now only reply directly to commands.`);
+            return message.reply({ embeds: [embed], ephemeral: true });
+        } 
+        // if channel exists, update musicchannel
+        const channels = client.channels.cache;
+        const result = channels.filter(channel => (channel.name === args[0] && channel instanceof TextChannel))
+
+        if(result.size === 0) {            
+            console.log("No text channel found")
+            const embed = new EmbedBuilder();
+            embed.setDescription(`No text channel found.`);
+            return message.reply({ embeds: [embed], ephemeral: true });
+        } else {
+            client.musicchannel = result.first()
+            const embed = new EmbedBuilder();
+            embed.setDescription(`Text channel found: ${args[0]}. ${client.user.username} now will play in channel "${args[0]}".`);
+            return message.reply({ embeds: [embed], ephemeral: true });
+        }
+
+    }
+})
